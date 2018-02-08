84c1abe14f21b2d55f5419c0e2c0ed34d103f8a7
CoryRodriguez <mrcoryrodriguez@gmail.com>
2018-02-07 19:51:17 -0600

    Finished UI and Functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,33 @@
 const guess = document.querySelector("#guess");
 const submit = document.querySelector("#submit").addEventListener("click", answer);
+const submitBtn = document.querySelector("#submit");
 const randomNumber = getRandomInt(11);
 const showAnswer = document.querySelector("#showAnswer");
-const guessesLeft = 3;
+const message = document.querySelector("#message");
+const game = document.querySelector("#game");
+let guessesLeft = 3;
 
 // Generate Random Number
 function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * 10) + 1 ;
 }
 
-
+// Play again event listener
+  game.addEventListener('mousedown', function(e){
+    if(e.target.value === 'Play Again?'){
+      window.location.reload();
+    }
+  })
 
 // Guess
 function answer(e){
   if (Number(guess.value) === randomNumber) {
-      correctAnswer()
-  } else if (guess.value === ''){
+    correctAnswer()
+    playAgain()
+  } else if (guess.value === '' || Number(guess.value) < 1 || Number(guess.value) > 10){
       empty();
   } else {
+    wrongNumber();
     console.log("try again");
   }
 
@@ -27,36 +37,88 @@ function answer(e){
 
 // If correct answer was guessed
 function correctAnswer() {
-  const game = document.getElementById("game");
-  const newP = document.createElement('p');
-  const content = document.createTextNode(randomNumber + " is correct!") // randomNumber + " is correct!"
-  const addP = newP.appendChild(content);
+  // Create p element
+  // const game = document.getElementById("game");
+  // const newP = document.createElement('p');
+  // const addP = newP.appendChild(content);
+  //const content = document.createTextNode(`${randomNumber} is correct!`) // randomNumber + " is correct!"
+  //const content = document.createTextNode(`${randomNumber} is correct!`) // randomNumber + " is correct!"
 
-  newP.className = 'text-success';
+
+  // Fill p element
+  //message.textContent = content;
+
+  //newP.className = 'text-success';
+
+  //game.appendChild(newP);
+
+  //submit.textContent = "Play Again";
+
+
+  setMessage(`${randomNumber} is correct, YOU WIN!`);
+  message.className = 'text-success';
   guess.className = 'form-control is-valid';
-  game.appendChild(newP);
 
-  //submit.textContent = "Play Again";
-
+  // Disable the input
+  guess.disabled = true;
 
   console.log('yup');
 }
 
+function setMessage(msg) {
+  message.textContent = msg;
+}
+
+// If empty, under 1 or over 10 
 function empty() {
-  const game = document.getElementById("game");
-  const newP = document.createElement('p');
-  const content = document.createTextNode("Please enter a number!") // randomNumber + " is correct!"
-  const addP = newP.appendChild(content);
+  // Create p element
+  // const game = document.getElementById("game");
+  // const newP = document.createElement('p');
+  // const content = document.createTextNode("Please enter a valid number!") // randomNumber + " is correct!"
+  // const addP = newP.appendChild(content);
+  setMessage(`Please enter a valid number!`);
 
-  newP.className = 'text-danger';
+  //newP.className = 'text-danger';
+  message.className = 'text-danger';
   guess.className = 'form-control is-invalid';
-  game.appendChild(newP);
+  //game.appendChild(newP);
 
   //submit.textContent = "Play Again";
 
 
   console.log('yup');
 }
+
+// Wrong number
+  function wrongNumber(){
+    guessesLeft -= 1;
+
+    if (guessesLeft === 0){
+      // Game over - lost
+      setMessage(`Game over, you lost. The correct number was ${randomNumber}!`);
+
+      message.className = 'text-danger';
+      guess.className = 'form-control is-invalid';
+      guess.disabled = true;
+      playAgain();
+
+    } else {
+      // Game continues - answer wrong
+      setMessage(`${guess.value} is not correct, ${guessesLeft} guesses left`);
+      message.className = 'text-danger';
+      guess.className = 'form-control is-invalid';
+
+      // Clear Input
+      guess.value = '';
+
+    }
+  }
+
+  // Play again
+  function playAgain(){
+    submitBtn.value = 'Play Again?';
+    submitBtn.className += ' play-again';
+  }
 
 
 console.log(randomNumber);
